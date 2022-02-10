f671c23af5123762a4008c2142554a26b7ecac8a
megfletch <mjfletcher@ucsb.edu>
2022-02-10 11:47:53 -0800

    Initial commit - reading in the data

---
+++ 00-load-libraries.R
@@ -0,0 +1,17 @@
+# 00-load-libraries.R
+# Loads case study summary document from the SNAPP-CRF Google Drive
+# case study summary analysis
+# M. Fletcher 2/10/2022
+
+library(here)
+library(tidyverse)
+library(ggplot2)
+library(RColorBrewer)
+library(janitor)
+library(googlesheets4)
+library(kableExtra)
+library(broom)
+library(ggpubr)
+library(psych)
+library(corrplot)
+library(cluster)
